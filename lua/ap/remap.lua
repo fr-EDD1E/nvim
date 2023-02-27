c6227cf709396d77626ff1be5986240b135586d7
guest <me@guest.com>
2023-02-27 12:00:01 +0100

    Some fiddled with remaps

--- lua/ap/remap.lua
+++ lua/ap/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>so", vim.cmd.so)
-vim.keymap.set({"n","t"}, "<leader>x", "<C-w>w")
+vim.keymap.set({"n","t"}, "<C-x>", "<C-w>w")
 vim.keymap.set("n", "<C-i>", "gg=G``")
 vim.keymap.set("n", "<leader>.", "gt")
 vim.keymap.set("n", "<leader>,", "gT")
