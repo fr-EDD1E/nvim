5dd1d85fff01f7ac0a4efddb236daa45f2cc2489
guest <me@guest.com>
2023-02-27 11:12:49 +0100

    Successfully added nvim-tree

---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>`",vim.cmd.NvimTreeToggle )
+vim.keymap.set("n", "<leader>r",vim.cmd.NvimTreeRefresh  )
--- lua/ap/set.lua
+++ lua/ap/set.lua
@@ -33,5 +33,39 @@ vim.opt.colorcolumn = "80"
 vim.g.mapleader = " "
 
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- OR setup with some options
+require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+})
 
 
